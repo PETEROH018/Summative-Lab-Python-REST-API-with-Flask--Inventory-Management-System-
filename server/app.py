5d1fc301276f321db39e1e05de56279ad73ed4a2
PETEROH018 <mathengep63@gmail.com>
2026-07-03 16:36:38 +0300

    Added a route that adds a new item to the inventory list

--- server/app.py
+++ server/app.py
@@ -16,5 +16,12 @@ def show_item(id):
     else:
         return jsonify({"error":f"No item with id:{id}"}),404
 
+@app.route('/inventory',methods = ['POST'])
+def add_item():
+    new_item = request.get_json()
+    id = max([item["id"] for item in items])+1
+    items.append({"id":id,"brands":new_item["brands"],"product_name":new_item["product_name"],"code":new_item["code"]})
+    return jsonify({"message":f"{new_item["product_name"]} has been succesfully added to the inventory list"}),201
+
 if __name__ == '__main__':
     app.run(debug=True)
--- server/data.py
+++ server/data.py
@@ -2,7 +2,7 @@ import requests
 
 def get_items():
         url = 'https://world.openfoodfacts.org/api/v2/search'
-        params = {"page_size":70,"fields":"code,product_name,brands"}
+        params = {"page_size":50,"fields":"code,product_name,brands"}
         headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"}
         response = requests.get(url,params=params,headers=headers)
         response.raise_for_status()
