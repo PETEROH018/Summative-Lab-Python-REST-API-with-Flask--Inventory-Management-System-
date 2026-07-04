6ddfe79636fbc6a0a9c53fedf1980b4791a5dfcc
PETEROH018 <mathengep63@gmail.com>
2026-07-04 23:23:19 +0300

    Added a function that handles displaying all items when a view-items CLI command is executed

---
+++ client/main.py
@@ -0,0 +1,18 @@
+import argparse
+from utils import view_items
+
+def main():
+    parser = argparse.ArgumentParser(description="Python-REST-API-with-Flask--Inventory-Management-System-")
+    subparsers = parser.add_subparsers()
+
+    view_items_subparser = subparsers.add_parser("view-items",help="view all items in the inventory")
+    view_items_subparser.set_defaults(func=view_items)
+
+    args = parser.parse_args()
+    if hasattr(args,"func"):
+        args.func(args)
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
