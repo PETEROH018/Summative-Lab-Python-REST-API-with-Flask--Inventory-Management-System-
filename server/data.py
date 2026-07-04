d2c1ca5c4b6ffb517cb6e5d370e0ea3bef654441
PETEROH018 <mathengep63@gmail.com>
2026-07-04 22:53:52 +0300

    Added a while loop to continously make requests to the API until a 200 response is obtained since the API keeps returning a 503 error at times

--- server/data.py
+++ server/data.py
@@ -5,10 +5,18 @@ def get_items():
         params = {"page_size":50,"fields":"code,product_name,brands"}
         headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"}
         response = requests.get(url,params=params,headers=headers)
-        response.raise_for_status()
-        items = [{**item,"id":index+1} for index,item in enumerate(response.json()["products"])]
-        return items
+        if response.ok:
+            items = [{**item,"id":index+1} for index,item in enumerate(response.json()["products"])]
+            return items
+        else:
+            return None
 
+while get_items() == None:
+     print(get_items())
+     get_items()
+     if get_items()!= None:
+          print(get_items())
+          break
 
 
 
