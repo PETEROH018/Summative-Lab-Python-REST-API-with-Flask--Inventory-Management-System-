c71631781831cba899ac9e8e571c90b0576d4e43
PETEROH018 <mathengep63@gmail.com>
2026-07-06 14:09:34 +0300

    updated the help description in some CLI commands

--- client/main.py
+++ client/main.py
@@ -5,10 +5,10 @@ def main():
     parser = argparse.ArgumentParser(description="Python-REST-API-with-Flask--Inventory-Management-System-")
     subparsers = parser.add_subparsers()
 
-    view_items_subparser = subparsers.add_parser("view-items",help="view all items in the inventory")
+    view_items_subparser = subparsers.add_parser("view-items",help="View all items in the inventory")
     view_items_subparser.set_defaults(func=view_items)
 
-    view_item_subparser = subparsers.add_parser("view-item",help="view an item's details by specifying its id")
+    view_item_subparser = subparsers.add_parser("view-item",help="View an item's details by specifying its id")
     view_item_subparser.add_argument('--id',help="Provide the id of the item you want to view which should be an integer")
     view_item_subparser.set_defaults(func=view_item)
 
